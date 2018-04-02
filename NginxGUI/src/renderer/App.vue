59b5855be4a37d9a2d274f73209cf0768f8f1ac9
Xheldon <c1006044256@gmail.com>
2018-04-02 19:04:12 +0800

    update logic

--- NginxGUI/src/renderer/App.vue
+++ NginxGUI/src/renderer/App.vue
@@ -1,15 +1,84 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <div id="control">
+            <button id="start">启动</button>
+            <button id="raload">重启</button>
+            <button id="stop">退出</button>
+            <button id="loadEBConfig">载入 EB 配置</button>
+            <button id="loadNewConfig">载入新的配置</button>
+            <button id="show">显示配置</button>
+        </div>
+        <div id="config">
+            <div>切换接口环境:</div>
+            <select v-model="current">
+                <option disabled value="">请选择</option>
+                <option v-for="(v, k) in switchList">{{v.sw}}</option>
+            </select>
+        </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'nginxgui'
-  }
+    import { NginxConfFile } from 'nginx-conf';
+    import configPath, { getConfList } from '@config/config.js';
+
+
+    export default {
+        name: 'Nginx-GUI',
+        data () {
+            return {
+                switchList: [{
+                    id: '1900-usdgk',
+                    sw: 'http://1900-usdgk-sl-eb.hotel.test.sankuai.com/'
+                }, {
+                    id: '1900-ualbf',
+                    sw: 'http://1900-ualbf-sl-eb.hotel.test.sankuai.com/'
+                }],
+                current: {}
+            }
+        },
+        watch: {
+            current (n, o) {
+                console.log(n, o);
+            }
+        },
+        methods: {
+            show () {
+                console.log(arr);
+            }
+        },
+        mounted () {
+            NginxConfFile.create('/usr/local/etc/nginx/nginx.conf', (err, conf) => {
+                if (err) {
+                    console.log('err:', err);
+                    return;
+                }
+                console.log('conf:', conf);
+                conf.nginx.http.server.forEach((v) => {
+                    if (v.server_name && v.server_name._value.includes('release.eb.sankuai.com')) {
+                        v.location.forEach((val)=> {
+                            if (val._value === '/api/v1/ebooking') {
+                                let value = val.proxy_pass._value;
+                                this.current = {
+                                    id: !value.indexOf('https') ? value.slice(0, 18) : value.slice(0, 17),
+                                    sw: value
+                                };
+                                console.log('current:', this.current);
+                                // 将配置写入文件:
+                                getConfList((err, conf) => {
+                                    if (err) {
+                                        shelljs.touch(configPath);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        }
+    }
 </script>
 
 <style>
-  /* CSS */
+    /* CSS */
 </style>
--- NginxGUI/src/renderer/main.js
+++ NginxGUI/src/renderer/main.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from 'vue';
+import axios from 'axios';
 
-import App from './App'
-import router from './router'
-import store from './store'
+import App from './App';
+import store from './store';
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.http = Vue.prototype.$http = axios
-Vue.config.productionTip = false
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+Vue.http = Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
+// 每次加载, 应该先读取已有的配置, 将值传递给组件
+
 
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
-  router,
-  store,
-  template: '<App/>'
-}).$mount('#app')
+    components: { App },
+    store,
+    template: '<App/>'
+}).$mount('#app');
